cad6b2007dd8b2e0fcf613c07e498e833a0c8dc3
Erwin Feng <efeng732@gmail.com>
2020-10-27 00:39:26 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,10 @@
 def find_max_value(array)
   count = 0 
   compare = 0 
-  while count <array.length do 
-    
-    
-    
-  counter += 1 
+  while count < array.length do 
+    if compare < array[counter]
+        compare = array[counter]
+      end 
+  counter += 1
+  compare 
 end
